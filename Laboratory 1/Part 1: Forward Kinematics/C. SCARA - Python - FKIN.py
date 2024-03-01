93d2a65b4933cd2152fcff24bdc456d9759aefd0
ken eje <157552825+EJEnyero-things@users.noreply.github.com>
2024-03-01 19:40:06 +0800

    Update C. SCARA - Python - FKIN.py

--- Laboratory 1/Part 1: Forward Kinematics/C. SCARA - Python - FKIN.py
+++ Laboratory 1/Part 1: Forward Kinematics/C. SCARA - Python - FKIN.py
@@ -2,3 +2,9 @@ import numpy as np
 import math
 
 #link lengths in mm
+
+a1 = float(input("a1 = "))
+a2 = float(input("a2 = "))
+a3 = float(input("a3 = "))
+a4 = float(input("a4 = "))
+a5 = float(input("a5 = "))
