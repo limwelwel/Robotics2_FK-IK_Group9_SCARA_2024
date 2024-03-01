e5ae45e7b26a6cecfa0e99b6a8b5c274a4b559f6
ken eje <157552825+EJEnyero-things@users.noreply.github.com>
2024-03-01 19:40:57 +0800

    Update C. SCARA - Python - FKIN.py

--- Laboratory 1/Part 1: Forward Kinematics/C. SCARA - Python - FKIN.py
+++ Laboratory 1/Part 1: Forward Kinematics/C. SCARA - Python - FKIN.py
@@ -8,3 +8,8 @@ a2 = float(input("a2 = "))
 a3 = float(input("a3 = "))
 a4 = float(input("a4 = "))
 a5 = float(input("a5 = "))
+
+#joint variables: is mm if f, is degrees if theta
+T1 = float(input("T1 = ")) #20 mm
+T2 = float(input("T2 = ")) #30 deg
+D3 = float(input("D3 = ")) #-90 deg
